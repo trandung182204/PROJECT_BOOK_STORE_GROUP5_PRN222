a6bcfe11714aa8078f0b2a1189196ae4107bf5be
trieuhoangson <sonthhe186811@fpt.edu.vn>
2025-10-14 22:53:17 +0700

    s

--- Repositories/CategoryRepository.cs
+++ Repositories/CategoryRepository.cs
@@ -1,20 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
-using PROJECT_BOOK_STORE_GROUP5_PRN222.Models;
+﻿//using Microsoft.EntityFrameworkCore;
+//using PROJECT_BOOK_STORE_GROUP5_PRN222.Models;
 
-namespace PROJECT_BOOK_STORE_GROUP5_PRN222.Repositories
-{
-    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
-    {
-        private readonly BookStoreContext _bookStoreContext;
-        public CategoryRepository(BookStoreContext context) : base(context) 
-        {
-            _bookStoreContext = context;
-        }
+//namespace PROJECT_BOOK_STORE_GROUP5_PRN222.Repositories
+//{
+//    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+//    {
+//        private readonly BookStoreContext _bookStoreContext;
+//        public CategoryRepository(BookStoreContext context) : base(context) 
+//        {
+//            _bookStoreContext = context;
+//        }
 
-        public async Task<Category?> GetCategoryByUserIdAsync(long userId)
-        {
-            return await _bookStoreContext.Categories.Include(c => c.BookCategories)
-                .FirstOrDefaultAsync(x => x.Id == userId);
-        }
-    }
-}
+//        public async Task<Category?> GetCategoryByUserIdAsync(long userId)
+//        {
+//            return await _bookStoreContext.Categories.Include(c => c.BookCategories)
+//                .FirstOrDefaultAsync(x => x.Id == userId);
+//        }
+//    }
+//}
